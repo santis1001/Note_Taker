82fe3bc5c9cc622fd370b7084aff3f9669fd7c3a
SV <72838881+santis1001@users.noreply.github.com>
2023-05-28 16:51:53 -0600

    Server and classes

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const api = require('./lib/routes/notes');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './lib/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './lib/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
