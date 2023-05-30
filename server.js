da04e3d1068e0e71f6bcc5e3fa77800cc883cd2c
SV <72838881+santis1001@users.noreply.github.com>
2023-05-29 22:18:33 -0600

    port PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const api = require('./lib/routes/notes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
